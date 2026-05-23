c6d30bc8b6b92d5e95f7d66dace7400ffa973ed6
[dyad] <reaganmungoma@gmail.com>
2026-05-23 19:42:08 +0200

    [dyad] Fixed infinite redirect loop causing blank page and infinite reload - wrote 3 file(s)

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,10 +1,5 @@
 import { defineConfig } from "nitro";
 
 export default defineConfig({
-  preset: "vercel",
-  publicAssets: [{ dir: "dist" }],
-  routeRules: {
-    "/**": { redirect: { to: "/index.html", statusCode: 200 } },
-    "/api/**": {},
-  },
+  preset: "static",
 });
